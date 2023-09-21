a34fef2042ffed49075030299d800684fdf1bac3
iamthepoe <luca1901p@gmail.com>
2023-09-20 23:21:47 -0300

    hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
+import 'react-awesome-button/dist/styles.css';
+import Banner from './components/Banner'
+import { AwesomeButton } from 'react-awesome-button'
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div className='main w-[100vw] h-[100vh] bg-black flex flex-col items-center '>
+        <Banner/>
+        <div className='buttons flex justify-around w-[100%]'>
+          <AwesomeButton type="danger">Jogue Agora 🎮</AwesomeButton>
+          <AwesomeButton type="danger">Saiba mais 🚀</AwesomeButton>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/components/Banner.tsx
@@ -0,0 +1,33 @@
+import '../App.css'
+import {TypeAnimation} from 'react-type-animation';
+
+function Banner() {
+  return (
+    <>
+      <div className='w-[100%] h-[50vh] flex justify-center items-center flex-col'>
+      <div className='logo'>
+        <h1 className='font-black text-white text-6xl'>
+            NINE<span className='text-red-600'>Z</span>
+        </h1>
+      </div>
+        <TypeAnimation
+            sequence={[
+                'As pétalas ainda sangram...',
+                  2000,
+                  'Um jogo da Gamejam:',
+                  2000,
+                  'A decadência das rosas 🌹',
+                  2000
+                ]}
+                speed={10}
+                className='text-red-600'
+                wrapper='span'
+            repeat={Infinity}
+        />
+      </div>
+    </>
+  )
+}
+
+export default Banner;
+
